bbeaca01ad70a5a0a06077e13ec51afa86c13751
陳文將 <d36584@gmail.com>
2018-11-28 17:31:39 +0800

    修改图片不会便小的bug

--- application/views/templates/wedding_feast.php
+++ application/views/templates/wedding_feast.php
@@ -41,7 +41,7 @@
         <div class="row d-flex justify-content-center"><img src="<?php echo base_url('assets/images/weddingFeast/titleFrame.png')?>"></div>
         <div class="row backgroundImg"><img src="<?php echo base_url('assets/images/weddingFeast/infoBackground.png')?>"></div>
             <div class="row">
-                <div class="col-lg-8 col-12 d-flex justify-content-center pl-lg-5 pt-lg-4 pb-lg-4"><img class="img-fluid" src="<?php echo base_url('assets/images/weddingFeast/2.png')?>"></div>
+                <div class="col-lg-8 col-12 d-flex justify-content-center pl-lg-5 pt-lg-4 pb-lg-4"><div><img class="img-fluid" src="<?php echo base_url('assets/images/weddingFeast/2.png')?>"></div></div>
                 <div class="col-lg-4 col-12">
                 <div class="col-12 d-flex justify-content-center pt-5"><div class="pt-4"><span class="textIconStyleWf">心心相印宴</span></div></div>
                 <div class="col-12 d-flex justify-content-center pt-4"><span style="color:#574800;font-size:20px;font-weight:bold;">RMB 5999元/桌起</span></div>
