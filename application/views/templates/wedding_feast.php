bb667405d8a8b0af384261d3a8ac2224c448b56c
陳文將 <d36584@gmail.com>
2018-12-10 17:51:25 +0800

    微调

--- application/views/templates/wedding_feast.php
+++ application/views/templates/wedding_feast.php
@@ -40,6 +40,15 @@
         font-weight:bold;
         margin-top:1.5rem;
     }
+    .feast-item{
+        background-image: url("<?php echo base_url('assets/images/weddingFeast/infoBackground.png')?>");
+        background-size: 100.5% 100.5%;
+        padding: 20px;
+        margin-bottom: 40px;
+        background-repeat: no-repeat;
+        background-position: center;
+        box-shadow: 0px 0px 10px 1px rgba(0,0,0,0.3);
+    }
     @media(max-width:1150px){
         .backgroundImg{
             display:none;
@@ -84,15 +93,7 @@
         }
     }
 
-    .feast-item{
-        background-image: url("<?php echo base_url('assets/images/weddingFeast/infoBackground.png')?>");
-        background-size: 100.5% 100.5%;
-        padding: 20px;
-        margin-bottom: 40px;
-        background-repeat: no-repeat;
-        background-position: center;
-        box-shadow: 0px 0px 10px 1px rgba(0,0,0,0.3);
-    }
+    
     .btn{
         transition:transform .1s ease-in;
         background:#5A4D18;
@@ -126,7 +127,7 @@
 <body>
     <div class="container">
         <div class="row d-flex justify-content-center pt-5"><span class="titleName">婚宴报价</span></div>
-        <div class="row d-flex justify-content-center pt-md-3 mb-5 titleFrame"><img src="<?php echo base_url('assets/images/weddingFeast/titleFrame.png')?>"></div>
+        <div class="pt-3 mb-5 titleFrame text-center"><img src="<?php echo base_url('assets/images/weddingFeast/titleFrame.png')?>"></div>
         <?php foreach ($feast as $key => $value) { ?>
             <div class="row feast-item">
                 <div class="col-lg-8 col-12">
